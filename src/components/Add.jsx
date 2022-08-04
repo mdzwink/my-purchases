5e21804c1e0540b34d7b63b3d983de6151b71721
Muhammad Zwink <mdzwink@gmail.com>
2022-08-04 00:54:40 -0400

    intitial commit with home page and navbar placeholders

---
+++ src/components/Add.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Add() {
+  return (
+    <>
+      <form >
+        <label>
+          Receipt:
+          <input type="text" name="new-receipt"/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+      <button>Add New receipt</button>
+    </>
+  )
+}
---
+++ src/components/View.jsx
@@ -0,0 +1,14 @@
+
+
+export default function View() {
+  return (
+    <>
+      <ul>
+        <li>Receipt 1</li>
+        <li>Receipt 2</li>
+        <li>Receipt 3</li>
+        <li>Receipt 4</li>
+      </ul>
+    </>
+  )
+}
