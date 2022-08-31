380f7a7ba698bd5bfe33059897b763e2347b00e4
mdzwink <mdzwink@gmail.com>
2022-08-31 10:43:05 -0400

    fix file structure issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
+import View from './components/View';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Welcome />
+      <View />
     </div>
   );
 }
---
+++ client/src/components/View.jsx
@@ -0,0 +1,9 @@
+import ReceiptList from './ReceiptList';
+
+export default function View() {
+  return (
+    <div className='view'>
+      <ReceiptList />
+    </div>
+  )
+}
